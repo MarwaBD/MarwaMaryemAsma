1da58f2220a8931e6d64c64e69c84c6301cc3916
MaryemBenMohamed <maryoumabac18@gmail.com>
2020-12-27 23:46:14 +0100

    commit java

---
+++ app/src/main/java/com/example/myapplication/HowToProtect.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HowToProtect extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_protect);
+    }
+}
